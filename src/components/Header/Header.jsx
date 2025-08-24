88e1fc7595715adef154ec058d28bab246846e2b
NovruzIbrahimov <novruz.ibrahimov56@gmail.com>
2025-08-24 19:29:19 +0400

    test15

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -50,7 +50,7 @@ function Header() {
 
   return (
     <header className={`custom-header ${theme}`}>
-      <div className="container d-flex justify-content-between align-items-center">
+      <div className="container d-flex align-items-center py-2">
         <Link to="/" className="header-left m-0" onClick={handleClick}>
           <h1 className="logo m-0">İbrahimov.dev</h1>
         </Link>
